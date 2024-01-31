f77b8ba704b32915597cb716860e7b4ee95f9c0b
WojciechNe <251192@edu.p.lodz.pl>
2024-01-31 13:07:10 +0100

    02 Dodajemy funkcje Mnozenie

--- main.cpp
+++ main.cpp
@@ -3,7 +3,6 @@
 int Mnozenie(int a, int b) {
     return a * b;
 }
-
 int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << "W pierwszym branchu" << Mnozenie(2,3)<< std::endl;
